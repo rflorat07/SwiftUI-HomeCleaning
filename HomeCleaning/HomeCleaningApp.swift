b487b50dc2ac80417dbafd4cbafb815eaf5b6ee3
Roger Florat Gutierrez <rflorat07@gmail.com>
2025-09-16 11:16:21 +0200

    Initial Commit

---
+++ HomeCleaning/HomeCleaningApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeCleaningApp.swift
+//  HomeCleaning
+//
+//  Created by Roger Florat Gutierrez on 16/09/25.
+//
+
+import SwiftUI
+
+@main
+struct HomeCleaningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
